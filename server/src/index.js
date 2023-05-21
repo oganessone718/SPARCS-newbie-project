fe103b1c6091e984a65186caf0f7f97828cfab75
oganessone718 <oganessone718@gmail.com>
2023-05-21 17:55:54 +0900

    Update server index

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,28 @@
-import express from "express";
-import dotenv from "dotenv";
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
 
+const app = express();
+const port = process.env.EXPRESS_PORT;
+
+const statusRouter = require('./routes/status');
+const mjRouter = require('./routes/mj');
+const accountRouter = require('./routes/account');
+
+
+app.use(express.json());
+
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log('[REQUEST-CORS] Request from origin: ', origin);
+        if (!origin || whitelist.indexOf(origin) !== -1) callback(null, true)
+        else callback(new Error('Not Allowed by CORS'));
+    },
+    credentials: true,
+}
 /* DO NOT REMOVE */
 /* Configure Environment Variables */
 if (process.env.ENVIRONMENT === "DEVELOPMENT") {
@@ -9,15 +31,25 @@ if (process.env.ENVIRONMENT === "DEVELOPMENT") {
 	dotenv.config({ path: ".env.production" })
 }
 
+app.use('/status', statusRouter);
+app.use('/feed', feedRouter);
+app.use('/account', accountRouter);
+app.use('/ssr', ssrRouter);
 
-const app = express();
-const port = process.env.EXPRESS_PORT;
+app.use('/static', express.static(path.join(__dirname,'public')));
 
-app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
+
+// Connect to MongoDB
+const OMongooseOption = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(process.env.MONGO_URI, OMongooseOption).then(
+    () => { console.log("[Mongoose] Connection Complete!") },
+    (err) => { console.log(`[Mongoose] Connection Error: ${ err }`) }
+);
 
 app.listen(port, () => {
 	console.log(`Express Listening @ http://localhost:${ port }`);
